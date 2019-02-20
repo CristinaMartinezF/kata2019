a2340db2ab9a2ffd8d34c9c0c343de2984ee1cf6
Cristina Martinez <cristinamartinezferro@hotmail.es>
2019-02-20 13:17:24 +0100

    contiene 3 devuelve Fizz

--- test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -48,4 +48,10 @@ public class TestFizzBuzz {
         String result = fb.getFizzBuzz(15);
         assertEquals("FizzBuzz", result);
     }
+     @Test
+    public void testcontiene3() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.getFizzBuzz(13);
+        assertEquals("Fizz", result);
+    }
 }
